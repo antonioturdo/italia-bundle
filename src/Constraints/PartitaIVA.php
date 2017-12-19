d30467932a6ee51b49e609ef4795f493f6e6c904
Antonio Turdo <antonio.turdo@gmail.com>
2017-12-19 21:58:43 +0100

    Added formal validation of PartitaIVA

--- src/Constraints/PartitaIVA.php
+++ src/Constraints/PartitaIVA.php
@@ -13,5 +13,20 @@ use Symfony\Component\Validator\Constraint;
  */
 class PartitaIVA extends Constraint {
     
-    public $message = 'This value is not a valid italian Partita IVA.';
+    public $message = null;
+    public $messageFormal = 'This value is not a formally valid italian Partita IVA.';
+    public $messageExistence = 'This value is not a currently existent italian Partita IVA.';
+    public $method = 'formal'; // or 'existence'
+    
+    public function __construct($options = null) {
+        parent::__construct($options);
+        
+        $methodsAvailable = array('formal', 'existence');
+        
+        if (!in_array($this->method, $methodsAvailable)) {
+            throw new \Symfony\Component\Validator\Exception\ValidatorException(
+                sprintf('The "method" option value for constraint PartitaIVA must be one of "%s"', implode('", "', $methodsAvailable))
+            );
+        }
+    }
 }
--- src/Constraints/PartitaIVAValidator.php
+++ src/Constraints/PartitaIVAValidator.php
@@ -10,15 +10,33 @@ use Symfony\Component\Validator\ConstraintValidator;
  *
  * @author Antonio Turdo <antonio.turdo@gmail.com>
  */
-class PartitaIVAValidator extends ConstraintValidator {
+class PartitaIVAValidator extends ConstraintValidator 
+{
 
     public function validate($value, Constraint $constraint)
     {
-        $validator = new \fdisotto\PartitaIVA($value, 'IT');    
-        if (!$validator->isValid()) {
-            $this->context->buildViolation($constraint->message)
+        $error = false;
+        switch ($constraint->method) {
+            case 'existence':
+                $validator = new \fdisotto\PartitaIVA($value, 'IT');    
+                if (!$validator->isValid()) {
+                    $message = is_null($constraint->message) ? $constraint->messageExistence : $constraint->message;
+                    $error = true;
+                }
+                break;
+            case 'formal':
+                $validator = new \sorciulus\PartitaIva\PartitaIva();
+                if (!$validator->setPartitaIva($value)->isValid()) {
+                    $message = is_null($constraint->message) ? $constraint->messageFormal : $constraint->message;
+                    $error = true;
+                }
+                break;
+        }
+        
+        if ($error) {   
+            $this->context->buildViolation($message)
                 ->setParameter('{{ string }}', $value)
-                ->addViolation();
+                ->addViolation();            
         }
     }
     
